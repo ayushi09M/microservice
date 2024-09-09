96d6c52f86b2280b5a27e168f21ea1d4d3565a1c
Ayushi <ayushi200malviya@gmail.com>
2024-09-09 23:19:49 +0530

    sql changes

--- src/main/resources/db/insert_product_table.sql
+++ src/main/resources/db/insert_product_table.sql
@@ -1,5 +1,7 @@
-INSERT INTO Product (name, quantity) VALUES ('Laptop', 50);
-INSERT INTO Product (name, quantity) VALUES ('Smartphone', 100);
-INSERT INTO Product (name, quantity) VALUES ('Tablet', 75);
-INSERT INTO Product (name, quantity) VALUES ('Headphones', 200);
-INSERT INTO Product (name, quantity) VALUES ('Smartwatch', 150);
+INSERT INTO Product (name, quantity)
+VALUES
+    ('Laptop', 50),
+    ('Smartphone', 100),
+    ('Tablet', 75),
+    ('Headphones', 200),
+    ('Smartwatch', 150);
